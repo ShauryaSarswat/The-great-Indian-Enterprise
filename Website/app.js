53473c5b25ab835ad511cd23d084c3196f3db31a
ShauryaSarswat <shauryasarswatpersonalAC@gmail.com>
2020-10-02 19:14:21 +0530

    Addition of a new delete button and Some of the major  interface changes
    The addition of a new delete button to delete the unwanted blogs and major designt changes to support the upcoming traffic.Addition of a little bootstrap .

--- Website/app.js
+++ Website/app.js
@@ -1,82 +1,74 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
-var methodOverride = require('method-override');
-const mongoose = require('mongoose');
+var express     = require("express"),
+methodOverride  = require("method-override"),
+bodyParser      = require("body-parser"),
+mongoose        = require("mongoose"),
+app             = express();
 mongoose.connect('mongodb://localhost:27017/restful_blog_app', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
-app.use(bodyParser.urlencoded({extended: true}));
-var blogSchema = {
-	title : String,
-	image :String,
-	body : String,
-	created  : {type : Date, default: Date.now}
-};
-var blog = mongoose.model("Blog", blogSchema);
-
-app.set("view engine","ejs");
-app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended : true}));
-app.use(methodOverride('_method'));
+app.set("view engine", "ejs"); //connecting ejs
+app.use(express.static("public")); //using express
+app.use(bodyParser.urlencoded({extended: true})); //using body-parser
+app.use(methodOverride("_method"));//using method-override + what to look for in url *the parentheses as above*
 //Routes
-
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: 
+        {type: Date, default: Date.now} //i.e it should be a date and to check for default date value as of now
+});
+var Blog = mongoose.model("Blog", blogSchema);
 // INDEX
 app.get("/",function(req, res){
-	res.redirect("/blogs");
+	res.render("landing");
 });
-app.get("/blogs", function(req, res){
- blog.find({}, function(err, blogs){
- if(err){
- 	console.log(err);
- }
- else{
- 	res.render("index", {blogs : blogs});
- }
- });
+app.get("/blogs", function (req, res){
+    Blog.find({}, function (err, blogs){ // adding index functionality to retrieve all blogs from database
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("index", {blogs: blogs}); //blogs:blogs -> render index with data (blogs is the data)
+        }
+    });
+});
+
+//CREATE ROUTE
+app.post("/blogs/new", function(req, res){
+   //create blog
+   Blog.create(req.body.blog, function (err, newBlog){
+       if(err) {
+           res.render("new");
+       } else {
+           //if successful, redirect to index
+           res.redirect("/blogs");
+       }
+   });
 });
 app.get("/blogs/new", function(req, res){
- res.render("new");
+    res.render("new");// all we have to do is render b/c its new
 });
-app.post("/blogs/new",function(req ,res){
-	blog.create(req.body.blog,function(err, newBlog){
-		if(err){
-			console.log(err);
-		}
-		else{
-			res.redirect("/blogs");
-		}
-	})
-});
-app.get("/blogs/:id",function(req, res){
-	blog.findById(req.params.id, function(err ,foundBlog){
-		if(err){
-			res.redirect("/blogs");
-		}
-		else{
-			res.render("show", {blog: foundBlog});
-		}
-	})
-});
-app.get("/blogs/:id/edit",function(req, res){
-	blog.findById(req.params.id, function(err ,foundBlog){
-		if(err){
-			res.redirect("/blogs/:id");
-		}
-		else{
-			res.render("edit", {blog: foundBlog});
-		}
-	})
-});
-app.put("/blogs/:id", function(req, res){
-   blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
-       if(err) {
+app.get("/blogs/:id", function(req, res){
+   Blog.findById(req.params.id, function(err, foundBlog){
+       if (err) {
            res.redirect("/blogs");
-           } else {
-               res.redirect("/blogs/" + req.params.id);
-           }
+       } else {
+           res.render("show", {blog: foundBlog});
+       }
+   })
+});
+
+app.delete("/blogs/:id", function(req, res){
+   //destroy blog
+   Blog.findByIdAndRemove(req.params.id, function(err){
+       if (err) {
+           res.redirect("/blogs");
+       } else {
+           res.redirect("/blogs");
+       }
    });
+   //redirect somewhere
 });
 app.listen(3000, function(){
 	console.log("server is running");
